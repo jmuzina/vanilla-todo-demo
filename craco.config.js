28fc79feeefaac48076b3ed0b693c2eb034484d9
Julie Muzina <jmuzina@jmuzina.io>
2024-02-20 20:59:23 -0500

    add craco, sass, vanilla, remove unneeded parts of cra boilerplate

---
+++ craco.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    webpack: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+        ]
+    }
+}
